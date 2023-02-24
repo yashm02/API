b78be6ca07b85c29c80cee01ef6157095df7c0f9
Yash Mittal <102669147+yashm02@users.noreply.github.com>
2023-02-25 02:20:57 +0530

    Update views.py

--- assignment/views.py
+++ assignment/views.py
@@ -1,6 +1,10 @@
 from rest_framework import generics
 from .models import Work, Artist
 from .serializers import WorkSerializer, ArtistSerializer
+from django.contrib.auth.models import User
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 class WorkList(generics.ListAPIView):
     serializer_class = WorkSerializer
@@ -23,3 +27,17 @@ class ArtistList(generics.ListAPIView):
         if artist_name is not None:
             queryset = queryset.filter(name__icontains=artist_name)
         return queryset
+
+@api_view(['POST'])
+def register(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    if not username or not password:
+        return Response({'error': 'Both username and password are required'}, status=status.HTTP_400_BAD_REQUEST)
+
+    try:
+        User.objects.create_user(username=username, password=password)
+        return Response({'success': 'User created successfully'}, status=status.HTTP_201_CREATED)
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
