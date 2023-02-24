1d4d1f3415c5daee9bfdd44110ca7e41b88733b6
Yash Mittal <102669147+yashm02@users.noreply.github.com>
2023-02-25 02:21:22 +0530

    Update urls.py

--- assignment/urls.py
+++ assignment/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import WorkList, ArtistList
+from .views import WorkList, ArtistList,register
 
 urlpatterns = [
+    path('register/',register,name="register"),
     path('works/', WorkList.as_view()),
     path('artists/', ArtistList.as_view()),
 ]
